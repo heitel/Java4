b4f16bdac08fa12f283b67f9f5156d57e57aef09
heitel <joachim@heitel.name>
2018-11-01 10:10:35 +0100

    tt

--- Fortuna/src/application/FortunaApp.java
+++ Fortuna/src/application/FortunaApp.java
@@ -1,26 +1,26 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
-
 
 public class FortunaApp extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = FXMLLoader.load(getClass().getResource("fortuna.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root, 600, 600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("WWI18SEB");
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
--- Fortuna/src/application/FortunaController.java
+++ Fortuna/src/application/FortunaController.java
@@ -1,24 +1,31 @@
 package application;
 
-import java.net.URL;
-
+import javafx.animation.RotateTransition;
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.util.Duration;
 
 public class FortunaController {
 	@FXML
-	private BorderPane bp;
-	private ResizeableImageView iv;
-
-	public FortunaController() {
-
+	private BorderPane main;
+	private ImageView pano = new ImageView(getClass().getResource("panorama.png").toString());
+	private ImageView pfeil = new ImageView(getClass().getResource("pfeil.png").toString());
+	
+	public FortunaController() {		
 	}
 	
 	@FXML
 	private void initialize() {
-		URL url = getClass().getResource("panorama.png");
-		iv = new ResizeableImageView(url.toString());
-		bp.setCenter(iv);
+		MyLayoutContainer container = new MyLayoutContainer(pano, pfeil);
+		main.setCenter(container);
 	}
 
+	@FXML
+	private void doFortuna() {
+		RotateTransition rotateTransition = new RotateTransition(Duration.millis(2000), pano);
+		double angle = (Math.random()-0.5) * 1440;
+		rotateTransition.setByAngle(angle);
+		rotateTransition.play();
+	}
 }
